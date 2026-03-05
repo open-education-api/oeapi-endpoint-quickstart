88a8068bb1adc38c0db7b985ed01d34ee4169128
Carlos Alonso Vega <carlos.alonso@unavarra.es>
2026-03-05 18:54:37 +0100

    Changes on install so test deployment behaves similar to prod or dev - own mysql data volume, isolated from dev or prod - follow similar pattern of deployment (e.g allowing switching to prod or dev easily)

--- start_oeapi.sh
+++ start_oeapi.sh
@@ -20,9 +20,10 @@ MAGENTA=$(tput setaf 5)
 CYAN=$(tput setaf 6)
 WHITE=$(tput setaf 7)
 
-# Project configuration folder
+# Project/src configuration folder
 CONF_SRC="./src/main/resources"
 
+# Config root folder
 CONFIG_TARGET_DIR="config"
 mkdir -p "$CONFIG_TARGET_DIR"
 
@@ -46,31 +47,53 @@ echo -ne "${RESET}${BOLD}${YELLOW}Your choice (1, 2, 3 or 4): ${RESET}"
 read choice
 
 
-# --- Lógica de Ejecución ---
+# --- Main menu ---
 
 case $choice in
   1)
     echo -e "\n${BOLD}${CYAN}Starting Demo mode...${RESET}"
+   
+    rm -f .env   # to avoid launching an unfinished install
+
+    # As there is no interactive configuration in this cas, let's handle
+    # some params needed to TEST mode to enable it to coexist with DEV or PROD      
+    # deployments if present or if you want to switch between them
+
     TARGET_DIR="$CONFIG_TARGET_DIR/test" 
     mkdir -p "$TARGET_DIR" 
+
     cp "$CONF_SRC/application.properties" "$TARGET_DIR/application-custom.properties"
+    cp "./examples/organizations.json"    "$TARGET_DIR/initial_org.json"
+    
+    # Change datasource to be follow pattern oeapi_qs_MODE, 
+    # So each MODE could have distinct dedicated volume in the container allowing switching and isolation
+    sed -i 's/\(spring.datasource.*\)oeapi_qs/\1oeapi_qs_test/' "$TARGET_DIR/application-custom.properties"
+    
+    # By default no orgs are loaded, let's put a default one. 
+    # initial_org.json usually stores a real org for DEV or PROD, which data is captured on guided install.
+    # In this case we previously put on initial_org.json use some orgs from ./examples
+    echo "quickdashboard.auto-create-file.organizations=${CONFIG_TARGET_DIR}/initial_org.json" >> "$TARGET_DIR/application-custom.properties"
+
     ENV_TARGET=test
     SERVER_PORT=57075
     echo "ENV_TARGET=$ENV_TARGET" > .env
     echo "SERVER_PORT=$SERVER_PORT" >> .env 
+
     docker compose up -d
     ;;
 
   2)
     echo -e "\n${BOLD}${CYAN}Let's customize Development mode...${RESET}"
+    rm -f .env   # to avoid launching an unfinished install
     ./prepare_params.sh dev    
     docker compose up -d --force-recreate
     ;;
+
   3)
     echo -e "\n${BOLD}${CYAN}Let's customize Production mode...${RESET}"
+    rm -f .env   # to avoid launching an unfinished install
     ./prepare_params.sh prod
     docker compose up -d --force-recreate
-    #docker compose build --no-cache && docker compose up -d
     ;;
 
   4)
