a4eba869b2e100da09929d741b9242bf25673393
Michiel de Mare <merloen@gmail.com>
2026-05-22 09:13:04 +0200

    GET program offerings

--- src/main/java/oeapi/controller/ProgramController.java
+++ src/main/java/oeapi/controller/ProgramController.java
@@ -4,6 +4,7 @@ import oeapi.controller.requestparameters.oeapiCourseRequestParam;
 import oeapi.controller.requestparameters.oeapiOfferingRequestParam;
 import oeapi.controller.requestparameters.oeapiProgramRequestParam;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -11,12 +12,19 @@ import java.util.Optional;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import oeapi.model.Course;
+import oeapi.model.Offering;
 import oeapi.model.Program;
+import oeapi.model.ProgramOffering;
 import oeapi.oeapiException;
+import oeapi.payload.ProgramOfferingDTO;
 import oeapi.service.CourseService;
+import oeapi.service.OfferingService;
 import oeapi.service.ProgramService;
+import oeapi.service.oeapiEnumConversionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -43,6 +51,15 @@ public class ProgramController extends oeapiDTOController<Program, ProgramDTO> i
 
     @Autowired
     private CourseService courseService;
+
+    @Autowired
+    private OfferingService offeringService;
+
+    @Autowired
+    private oeapiEnumConversionService enumService;
+
+    @Value("${ooapi.config.autoCreateOfferingIfNotExists:false}")
+    private boolean defaultAutoCreateOfferingIfNotExists;
 
     @GetMapping
     public ResponseEntity<?> getAll(@ModelAttribute oeapiProgramRequestParam requestParam) {
@@ -72,6 +89,36 @@ public class ProgramController extends oeapiDTOController<Program, ProgramDTO> i
         }
 
         throw new oeapiException(HttpStatus.NOT_FOUND, "There are not courses for Id: " + id);
+    }
+
+    @GetMapping(value = "/{id}/offerings", produces = "application/json")
+    public ResponseEntity<?> getOfferings(@PathVariable String id, @ModelAttribute oeapiOfferingRequestParam requestParam) {
+
+        Optional<Program> existing = service.getById(id);
+        if (!existing.isPresent()) {
+            throw new oeapiException(HttpStatus.NOT_FOUND, "There are no program-offerings for Id: " + id);
+        }
+        List<ProgramOffering> programOfferings = offeringService.getByProgramId(id);
+
+        // If there is no ProgramOffering and Autocreate is on, return a basic autogen offering
+        if (programOfferings.isEmpty()) {
+            List<Offering> offerings = new ArrayList<>();
+            if (defaultAutoCreateOfferingIfNotExists) {
+                offerings.add(offeringService.autoGenerateBasicItem(id));
+            }
+            return super.getResponse(requestParam, offerings);
+        }
+        oeapiDTOMapper<ProgramOffering, ProgramOfferingDTO> mapper =
+                new oeapiDTOMapper<ProgramOffering, ProgramOfferingDTO>(ProgramOffering.class,
+                        ProgramOfferingDTO.class,
+                        enumService,
+                        Arrays.asList());
+
+        List<ProgramOfferingDTO> dtos = mapper.toDTOList(programOfferings);
+        Pageable pageable = requestParam.toPageable();
+        oeapiResponse<ProgramOfferingDTO> response = new oeapiResponse<ProgramOfferingDTO>(dtos, pageable);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping
